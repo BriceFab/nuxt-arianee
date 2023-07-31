f25ab6f39db6eb8a40f81644c4692d319d2a3eb5
Fabrice Riva <fabrice.riva@iomedia.ch>
2023-07-31 17:25:17 +0200

    align babel config
    Co-authored-by: Fabrice Riva <fabrice.riva@iomedia.ch>

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,5 +65,102 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+    babel: {
+      babelrc: false,
+      cacheDirectory: undefined,
+      presets({ envName, isServer, isClient, isModern, isLegacy }) {
+        const targets = {
+          client: { browsers: [
+              "defaults",
+              "not dead",
+              "not op_mini all",
+            ],
+            // ie: 11
+          },
+          server: { node: 'current' },
+        };
+        const polyfills = {
+          legacy: [
+            'es.array.iterator',
+            'es.array.includes',
+            'es.array.for-each',
+            'es.array.find',
+            'es.array.find-index',
+            'es.array.from',
+            'es.array.some',
+            'es.array.some',
+            'es.typed-array.iterator',
+            'es.promise',
+            'es.promise.finally',
+            'es.object.assign',
+            'es.object.keys',
+            'es.object.values',
+            'es.object.entries',
+            'es.object.has-own',
+            'es.string.starts-with',
+            'es.string.ends-with',
+            'es.string.pad-start',
+            'es.string.pad-end',
+            'es.string.includes',
+            'es.string.repeat',
+            'es.symbol',
+            'es.symbol.iterator',
+            'es.symbol.async-iterator',
+            'es.symbol.search',
+            'es.symbol.has-instance',
+            'es.symbol.is-concat-spreadable',
+            'es.symbol.search',
+            'es.symbol.match',
+            'es.symbol.replace',
+            'es.symbol.species',
+            'es.symbol.to-primitive',
+            'es.symbol.split',
+            'es.symbol.description',
+            'es.symbol.to-string-tag',
+            'es.symbol.unscopables',
+            'es.math.to-string-tag',
+            'es.map',
+            'es.set',
+            'es.weak-map',
+            'es.weak-set',
+            'es.weak-set',
+            'es.number.is-integer',
+            'web.dom-collections.iterator',
+            'web.dom-collections.for-each',
+            'web.url',
+            'web.url-search-params',
+          ],
+          modern: [
+          ]
+        };
+        return [
+          [
+            require.resolve('@nuxt/babel-preset-app'),
+            // '@nuxt/babel-preset-app-edge' // For nuxt-edge users
+            {
+              buildTarget: isServer ? 'server' : 'client',
+              polyfills: isClient && isLegacy && !isModern ? polyfills.legacy : polyfills.modern,
+              useBuiltIns: 'usage',
+              targets: isServer ? targets.server : targets.client,
+              corejs: {
+                version: '3.18',
+                proposals : true
+              },
+              shippedProposals: true
+            }
+          ]
+        ];
+      },
+    },
+    transpile: ['@arianee/arn-client', '@arianee/arn-types', '@arianee/arn-components', '@walletconnect', '@web3modal'],
+    postcss: {
+      plugins: {
+        autoprefixer: {},
+        tailwindcss: {},
+        // Désactiver `postcss-url`
+        'postcss-url': false,
+      },
+    },
+    plugins: [],
+  },
 }
